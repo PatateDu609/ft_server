14eab7562ecbf0ef00811c7e6e2c5fe064dc6645
Ghali Boucetta <patate.du.609@gmail.com>
2019-12-15 02:40:18 +0100

    mysql still needs to be fixed

--- Dockerfile
+++ Dockerfile
@@ -6,21 +6,23 @@
 #    By: gboucett <gboucett@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/12/11 14:36:16 by gboucett          #+#    #+#              #
-#    Updated: 2019/12/12 02:07:02 by gboucett         ###   ########.fr        #
+#    Updated: 2019/12/15 02:12:09 by gboucett         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 FROM debian:buster
 
-RUN apt-get update 
+RUN apt-get update && apt-get upgrade -y
 RUN apt-get install -y nginx
 RUN apt-get install -y mariadb-server
-RUN apt-get install -y php-fpm php-mysql php-json
+RUN apt-get install -y php7.3 php7.3-fpm php7.3-mysql php-common php7.3-cli php7.3-common php7.3-json php7.3-opcache php7.3-readline
 
 EXPOSE 80
 
-COPY srcs/wordpress /var/www/localhost
-COPY srcs/pma /var/www/phpmyadmin
+COPY srcs/wordpress.tar.gz /root
+COPY srcs/pma.tar.gz /root
+COPY srcs/config_nginx /root
+COPY srcs/config.inc.php /root
 COPY srcs/start.sh /usr/bin
 RUN chmod u+x /usr/bin/start.sh
 
--- srcs/start.sh
+++ srcs/start.sh
@@ -1,4 +1,38 @@
 #!/bin/bash
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    start.sh                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: gboucett <gboucett@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/12/14 22:53:55 by gboucett          #+#    #+#              #
+#    Updated: 2019/12/14 22:54:00 by gboucett         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
 
-cd /var/www/localhost
-ls
+WEBDIR="/var/www/boucetta.com"
+NGINX_CONF_LNK="/etc/nginx/sites-enabled/boucetta.com.conf"
+NGINX_CONF="/etc/nginx/sites-available/boucetta.com.conf"
+
+cd /root
+echo "Setting up nginx config file"
+rm /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
+mv config_nginx $NGINX_CONF
+ln -s $NGINX_CONF $NGINX_CONF_LNK
+
+echo "Installing Phpmyadmin and Wordpress"
+tar xf pma.tar.gz
+tar xf wordpress.tar.gz
+rm -rf pma.tar.gz wordpress.tar.gz
+mv wordpress $WEBDIR
+mv pma $WEBDIR/phpmyadmin
+
+echo "Setting up Phpmyadmin"
+mv config.inc.php $WEBDIR/phpmyadmin
+
+echo "Launching services"
+service mysql start
+service php7.3-fpm start
+# service nginx start
+nginx -g 'daemon off;'
